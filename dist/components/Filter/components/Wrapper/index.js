e2b8b559ac5d21de7d0fa66835fb008924c46611
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-09-26 11:21:38 -0300

    chore(#10): Add build files

--- dist/components/Filter/components/Wrapper/index.js
+++ dist/components/Filter/components/Wrapper/index.js
@@ -2,9 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Wrapper = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
-const useFields_1 = require("../../hooks/useFields");
 const useFilter_1 = require("../../hooks/useFilter");
 const Wrapper = ({ children, register }) => {
-    return ((0, jsx_runtime_1.jsx)(useFilter_1.FilterProvider, { register: register, children: (0, jsx_runtime_1.jsx)(useFields_1.FilterFieldsProvider, { children: children }) }));
+    return (0, jsx_runtime_1.jsx)(useFilter_1.FilterProvider, { register: register, children: children });
 };
 exports.Wrapper = Wrapper;
