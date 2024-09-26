ec8a70ca4e938a7bbc332c3ff3c4aaf6f9aa5e61
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-09-26 14:17:39 -0300

    feat(#10): Add missing filter exports

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Dropdown = exports.Badge = exports.Button = void 0;
+exports.useFilterFields = exports.useFilter = exports.useFilterContext = exports.Filter = exports.Dropdown = exports.Badge = exports.Button = void 0;
 var Button_1 = require("./components/Button");
 Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.Button; } });
 var Badge_1 = require("./components/Badge");
 Object.defineProperty(exports, "Badge", { enumerable: true, get: function () { return Badge_1.Badge; } });
 var Dropdown_1 = require("./components/Dropdown");
 Object.defineProperty(exports, "Dropdown", { enumerable: true, get: function () { return Dropdown_1.Dropdown; } });
+var Filter_1 = require("./components/Filter");
+Object.defineProperty(exports, "Filter", { enumerable: true, get: function () { return Filter_1.Filter; } });
+var useFilter_1 = require("./components/Filter/hooks/useFilter");
+Object.defineProperty(exports, "useFilterContext", { enumerable: true, get: function () { return useFilter_1.useFilterContext; } });
+Object.defineProperty(exports, "useFilter", { enumerable: true, get: function () { return useFilter_1.useFilter; } });
+var useFields_1 = require("./components/Filter/hooks/useFields");
+Object.defineProperty(exports, "useFilterFields", { enumerable: true, get: function () { return useFields_1.useFilterFields; } });
