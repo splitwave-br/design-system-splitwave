906b443b3c32ff2298581a4a5bd0d895585bbec1
ekSayuri <esayu.nishimura96@gmail.com>
2024-10-03 16:47:12 -0300

    fix(#18): Fix missing Icon export

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Header = exports.Cell = exports.Table = exports.useFilterFields = exports.useFilter = exports.useFilterContext = exports.Filter = exports.Dropdown = exports.Badge = exports.Button = void 0;
+exports.Icon = exports.Header = exports.Cell = exports.Table = exports.useFilterFields = exports.useFilter = exports.useFilterContext = exports.Filter = exports.Dropdown = exports.Badge = exports.Button = void 0;
 // Button
 var Button_1 = require("./components/Button");
 Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return Button_1.Button; } });
@@ -23,3 +23,6 @@ var Table_1 = require("./components/Table");
 Object.defineProperty(exports, "Table", { enumerable: true, get: function () { return Table_1.Table; } });
 Object.defineProperty(exports, "Cell", { enumerable: true, get: function () { return Table_1.Cell; } });
 Object.defineProperty(exports, "Header", { enumerable: true, get: function () { return Table_1.Header; } });
+// Icon
+var Icon_1 = require("./components/Icon");
+Object.defineProperty(exports, "Icon", { enumerable: true, get: function () { return Icon_1.Icon; } });
