8dc2d91094e17e38a6eb9a60c042a3a2f5a0a8ae
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-11-22 10:14:59 -0300

    feat(tabs): Add related path prop to trigger

--- dist/components/Tabs/components/trigger/trigger.js
+++ dist/components/Tabs/components/trigger/trigger.js
@@ -4,9 +4,9 @@ import { usePathname } from "next/navigation";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 var Trigger = function (_a) {
-    var path = _a.path, children = _a.children, _b = _a.isDisabled, isDisabled = _b === void 0 ? false : _b, currentPath = _a.currentPath;
+    var path = _a.path, children = _a.children, _b = _a.isDisabled, isDisabled = _b === void 0 ? false : _b, currentPath = _a.currentPath, relatedPath = _a.relatedPath;
     var pathName = currentPath || usePathname();
-    var isActive = pathName === path;
+    var isActive = pathName === path || (relatedPath && pathName.includes(relatedPath));
     var tabStyles = [
         styles.trigger,
         isActive && styles.active,
