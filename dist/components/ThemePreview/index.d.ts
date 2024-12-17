1c3c74d7e325e8296c01d638dfdb9dcf2de8ba5b
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-12-17 10:37:47 -0300

    fix(#188): Add missing build files

--- dist/components/ThemePreview/index.d.ts
+++ dist/components/ThemePreview/index.d.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 type TProps = {
     children: ReactNode;
+    showMenu?: boolean;
 };
-export declare const ThemePreview: ({ children }: TProps) => import("react/jsx-runtime").JSX.Element;
+export declare const ThemePreview: ({ children, showMenu }: TProps) => import("react/jsx-runtime").JSX.Element;
 export {};
