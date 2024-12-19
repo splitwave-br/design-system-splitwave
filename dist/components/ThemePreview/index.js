41555f2704f4bc38f19473af51e9f74a17de9aa1
Dener Oliveira <denerdev013@gmail.com>
2024-12-19 10:52:21 -0300

    feat(#188): fix menu index

--- dist/components/ThemePreview/index.js
+++ dist/components/ThemePreview/index.js
@@ -2,6 +2,6 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import styles from "./styles.module.scss";
 import classnames from "classnames";
 export var ThemePreview = function (_a) {
-    var children = _a.children;
-    return (_jsxs("div", { className: styles.wrapper, children: [_jsxs("div", { className: classnames("light-theme", styles.lightTheme), children: [_jsx("h2", { className: styles.title, children: "Light Theme" }), children] }), _jsxs("div", { className: classnames("dark-theme", styles.darkTheme), children: [_jsx("h2", { className: styles.title, children: "Dark Theme" }), children] })] }));
+    var children = _a.children, showMenu = _a.showMenu;
+    return (_jsxs("div", { className: styles.wrapper, children: [_jsxs("div", { className: classnames("light-theme", styles.lightTheme), children: [_jsx("h2", { className: styles.title, children: "Light Theme" }), children] }), _jsxs("div", { style: { marginTop: showMenu ? 200 : 0 }, className: classnames("dark-theme", styles.darkTheme), children: [_jsx("h2", { className: styles.title, children: "Dark Theme" }), children] })] }));
 };
