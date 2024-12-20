34379c1d777217fa8b13c08a37e31e76863a0835
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-12-20 11:36:10 -0300

    fix(#165): Fix click outside logic

--- dist/hooks/useClickOutside.js
+++ dist/hooks/useClickOutside.js
@@ -6,8 +6,8 @@ var useClickOutside = function (_a) {
             var _a;
             var target = event.target;
             var exceptionHasTarget = (exceptionRef === null || exceptionRef === void 0 ? void 0 : exceptionRef.current) && ((_a = exceptionRef === null || exceptionRef === void 0 ? void 0 : exceptionRef.current) === null || _a === void 0 ? void 0 : _a.contains(target));
-            var refHasTarget = ref.current && !ref.current.contains(target);
-            if (!exceptionHasTarget || !refHasTarget) {
+            var refHasTarget = ref.current && ref.current.contains(target);
+            if (!exceptionHasTarget && !refHasTarget) {
                 callback();
             }
         };
