fec04fc9becdbd9a0461b4c5a5562e7b020028d2
Dener Oliveira <denerdev013@gmail.com>
2025-01-14 11:14:19 -0300

    feat(#216): added support on badge default

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Badge } from './index'
+import { ThemePreview } from "../ThemePreview";
 
 const meta = {
   component: Badge,
@@ -13,7 +14,42 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     size: "sm",
+    children: "Badge",
+  }
+};
+
+export const ThemeSupport: Story = {
+  render: ({ children, ...args }) => (
+    <ThemePreview>
+      <Badge>{children}</Badge>
+    </ThemePreview>
+  ),
+  args: {
+    size: "sm",
+    children: "Badge",
+  }
+};
+
+export const Blue: Story = {
+  args: {
+    size: "sm",
     variant: "blue",
+    children: "Badge",
+  }
+};
+
+export const Indigo: Story = {
+  args: {
+    size: "sm",
+    variant: "indigo",
+    children: "Badge",
+  }
+};
+
+export const Success: Story = {
+  args: {
+    size: "sm",
+    variant: "success",
     children: "Badge",
   }
 };
