4a391a3e6c4bf93c2bdfb6f8a834cb09c2568d4a
Dener Oliveira <denerdev013@gmail.com>
2025-02-14 11:33:07 -0300

    chore(icon-dark-light): fixed attributes

--- src/components/Icon/components/Sun.tsx
+++ src/components/Icon/components/Sun.tsx
@@ -10,7 +10,7 @@ export const Sun = () => {
         <path
           d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z"
           stroke="currentColor"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
